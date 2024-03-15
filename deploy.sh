27c01354700afc0e9492787ba24a3743675ab216
Frank Villaro-Dixon <frank@villaro-dixon.eu>
2024-03-15 01:45:29 +0100

    move things around
    Signed-off-by: Frank Villaro-Dixon <frank@villaro-dixon.eu>

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-files=(vyatta/sbin/ubnt-multi-table.pl vyatta/share/vyatta-cfg/templates/protocols/static/table/node.tag/route6/node.tag/next-hop/node.def vyatta/share/vyatta-cfg/templates/protocols/static/table/node.tag/route6/node.tag/next-hop/node.tag/interface/node.def)
-#files=(vyatta/sbin/ubnt-multi-table.pl)
-files=(vyatta/share/vyatta-cfg/templates/protocols/static/table/node.tag/route6/node.tag/next-hop/node.tag/interface/node.def)
+files=(opt/vyatta/sbin/ubnt-multi-table.pl opt/vyatta/share/vyatta-cfg/templates/protocols/static/table/node.tag/route6/node.tag/next-hop/node.def opt/vyatta/share/vyatta-cfg/templates/protocols/static/table/node.tag/route6/node.tag/next-hop/node.tag/interface/node.def)
 
 
 for f in ${files[@]}; do
 	echo ">>$f";
 	bn=$(basename $f)
 	scp $f ubnt@192.168.10.254:/tmp/$bn
-	ssh ubnt@192.168.10.254 "sudo mv /tmp/$bn /opt/$f"
+	ssh ubnt@192.168.10.254 "sudo mv /tmp/$bn /$f"
 done
